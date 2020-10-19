53656e43b812bccc56f002b73a3c712d18e7324a
Lukas-De-Angelis-Riva <ldeangelis@fi.uba.ar>
2020-10-19 12:42:27 -0300

    Están disponibles todas las utilidades del tp

--- TP0/TP0.c
+++ TP0/TP0.c
@@ -8,61 +8,12 @@
 
 #define MAXIMO_ARCHIVO 100
 
-size_t obtener_tamanio_encode(size_t tamanio){
-	switch(tamanio%3){
-		case 0:
-			return tamanio*4/3 + 1;
-		break;
-		case 1:
-			return (tamanio+2)*4/3 + 1;
-		break;
-		case 2:
-			return (tamanio+1)*4/3 + 1;
-		break;
-	}
-	return 0;
-}
-
+/* DECODE */
 size_t obtener_tamanio_decode(size_t tamanio){
 	return tamanio*3/4 + 1;
 }
 
-void codificar(char nombre_fentrada[MAXIMO_ARCHIVO], char nombre_fsalida[MAXIMO_ARCHIVO]){
-	FILE* fentrada = fopen(nombre_fentrada, "r");
-	if(!fentrada){
-		return;
-	}
-	FILE* fsalida = fopen(nombre_fsalida, "w");
-	if(!fsalida){
-		fclose(fentrada);
-		return;
-	}
-
-	fseek(fentrada, 0, SEEK_END); 
-	size_t size = (size_t) ftell(fentrada);
-	fseek(fentrada, 0, SEEK_SET); 
-
-	char* entrada = calloc(size + 1, sizeof(char));
-	if(!entrada){
-		fclose(fentrada);
-		fclose(fsalida);
-		return;
-	}
-
-	fread(entrada, 1, size, fentrada);
-
-	char* salida = calloc(obtener_tamanio_encode(size), sizeof(char));
-
-	codificador64((unsigned char*)entrada, salida);
-	
-	fwrite(salida, 1, obtener_tamanio_encode(size), fsalida);
-
-	fclose(fentrada);
-	fclose(fsalida);
-	free(entrada);
-}
-
-void decodificar(char nombre_fentrada[MAXIMO_ARCHIVO], char nombre_fsalida[MAXIMO_ARCHIVO]){
+void decodificar_archivo_archivo(char nombre_fentrada[MAXIMO_ARCHIVO], char nombre_fsalida[MAXIMO_ARCHIVO]){
 	FILE* fentrada = fopen(nombre_fentrada, "r");
 	if(!fentrada){
 		return;
@@ -97,108 +48,302 @@ void decodificar(char nombre_fentrada[MAXIMO_ARCHIVO], char nombre_fsalida[MAXIM
 	free(entrada);
 }
 
+void decodificar_archivo_stdout(char nombre_fentrada[MAXIMO_ARCHIVO]){
+	FILE* fentrada = fopen(nombre_fentrada, "r");
+	if(!fentrada){
+		return;
+	}
+	fseek(fentrada, 0, SEEK_END);
+	size_t size = (size_t) ftell(fentrada);
+	fseek(fentrada, 0, SEEK_SET);
 
-void leer_stdin_encode(){
-	int cant_leidos = 30;
-	do{
-		char array_aEnco[4] = "";
-		char array_encodificado[5] = "";
-		cant_leidos = scanf("%c%c%c", &array_aEnco[0], &array_aEnco[1], &array_aEnco[2]);
-		if(cant_leidos == 1){
-			array_aEnco[1] = '\0';
-			array_aEnco[2] = '\0';
-		}
-		if(cant_leidos == 2){
-			array_aEnco[2] = '\0';
-		}
-		codificador64((unsigned char*)array_aEnco, array_encodificado);
-		printf("%s", array_encodificado);
-	}while(cant_leidos == 3);
+	char* entrada = calloc(size + 1, sizeof(char));
+	if(!entrada){
+		fclose(fentrada);
+		return;
+	}
+
+	fread(entrada, 1, size, fentrada);
+
+	char* salida = calloc(obtener_tamanio_decode(size), sizeof(char));
+
+	decodificador64((unsigned char*)entrada, salida);
+
+	printf("%s",salida);
+
+	fclose(fentrada);
+	free(entrada);
 }
 
-void leer_stdin_decode(){
-	int cant_leidos = 30;
+void decodificar_stdin_archivo(char nombre_fsalida[MAXIMO_ARCHIVO]){
+	FILE* fsalida = fopen(nombre_fsalida, "w");
+	if(!fsalida){
+		// TODO: ESCRIBIR ALGO.
+		return;
+	}
+
+	bool sigo_leyendo = true;
 	do{
 		char array_aDeco[5] = "";
 		char array_decodificado[4] = "";
-		cant_leidos = scanf("%c%c%c%c", &array_aDeco[0], &array_aDeco[1], &array_aDeco[2], &array_aDeco[3]);
-		decodificador64((unsigned char*)array_aDeco, array_decodificado);
-		printf("%s", array_decodificado);
-	}while(cant_leidos == 4);
-}
-
-
-void mostrar_por_pantalla(char* argumento){
-	size_t tamanio = strlen(argumento);
-	size_t tamanio_encodificado = obtener_tamanio_encode(tamanio);
-	char* encodificado = calloc(tamanio_encodificado, sizeof(char));
-	codificador64((unsigned char*)argumento, encodificado);
-	printf("%s", encodificado);
-}
-
-void mostrar_ayudas(){
-	printf("No podés solo, necesitás ayudas.\n"); // :TODO
-}
-
-void mostrar_version(){
-	printf("Pediste la versión.\n");
-}
-
-int main(int argc, char** argv){
-
-	if(argc == 2){
-		if(argv[1][0] == '-' && argv[1][1] == 'd'){
-			leer_stdin_decode();
-		}
-	}else if(argc == 1){
-		leer_stdin_encode();
-	}else{
-		int opt;
-
-		char archivo_entrada[MAXIMO_ARCHIVO] = "";
-		char archivo_salida[MAXIMO_ARCHIVO] = "";
-		bool decode = false;
-
-		while((opt = getopt(argc, argv, "hi:o:dv")) != -1) {
-			switch(opt){
-				case 'h':
-					mostrar_ayudas();
-				break;
-				case 'v':
-					mostrar_version();
-				break;
-
-				case 'i':
-					strcpy(archivo_entrada, optarg);
-				break;
-				case 'o':
-					strcpy(archivo_salida, optarg);
-				break;
-				case 'd':
-					decode = true;
-				break;
-				default:
-				break;
-			}
-		}
-
-		if(strlen(archivo_entrada) == 0 || strlen(archivo_salida) == 0){
-			printf("F\n");
-		}
-
-		if(decode){
-			decodificar(archivo_entrada, archivo_salida);
+		scanf("%c%c%c%c", &array_aDeco[0], &array_aDeco[1], &array_aDeco[2], &array_aDeco[3]);
+		if(strlen(array_aDeco) == 0){
+			sigo_leyendo = false;
+		}else if(strlen(array_aDeco) < 4){
+			printf("Entrada invalida. La entrada deberia ser multiplo de 4\n");
+			sigo_leyendo = false;
 		}else{
-			codificar(archivo_entrada, archivo_salida);
+			decodificador64((unsigned char*)array_aDeco, array_decodificado);
+			fwrite(array_decodificado, 1, obtener_tamanio_decode(strlen(array_decodificado)), fsalida);
 		}
-		return 0;
-	}
+	}while(sigo_leyendo);
+}
 
-	printf("\n");
+void decodificar_stdin_stdout(){
+	bool sigo_leyendo = true;
+	do{
+		char array_aDeco[5] = "";
+		char array_decodificado[4] = "";
+		scanf("%c%c%c%c", &array_aDeco[0], &array_aDeco[1], &array_aDeco[2], &array_aDeco[3]);
+		if(strlen(array_aDeco) == 0 || (strlen(array_aDeco) == 1 && array_aDeco[0] == '\n')){
+			sigo_leyendo = false;
+		}else if(strlen(array_aDeco) < 4){
+			printf("Entrada invalida. La entrada deberia ser multiplo de 4\n");
+			sigo_leyendo = false;
+		}else{
+			decodificador64((unsigned char*)array_aDeco, array_decodificado);
+			printf("%s", array_decodificado);
+		}
+	}while(sigo_leyendo);
+}
+
+
+/* ENCODE */
+size_t obtener_tamanio_encode(size_t tamanio){
+	switch(tamanio%3){
+		case 0:
+			return tamanio*4/3 + 1;
+		break;
+		case 1:
+			return (tamanio+2)*4/3 + 1;
+		break;
+		case 2:
+			return (tamanio+1)*4/3 + 1;
+		break;
+	}
 	return 0;
 }
 
+void codificar_archivo_archivo(char nombre_fentrada[MAXIMO_ARCHIVO], char nombre_fsalida[MAXIMO_ARCHIVO]){
+	FILE* fentrada = fopen(nombre_fentrada, "r");
+	if(!fentrada){
+		printf("No se pudo abrir al archivo de entrada.\n");
+		return;
+	}
+	FILE* fsalida = fopen(nombre_fsalida, "w");
+	if(!fsalida){
+		fclose(fentrada);
+		return;
+	}
 
-/*STDIN Es la entrada por consola.
+	fseek(fentrada, 0, SEEK_END); 
+	size_t size = (size_t) ftell(fentrada);
+	fseek(fentrada, 0, SEEK_SET); 
 
-STDOUT es la salida por consola.*/
+	char* entrada = calloc(size + 1, sizeof(char));
+	if(!entrada){
+		fclose(fentrada);
+		fclose(fsalida);
+		return;
+	}
+
+	fread(entrada, 1, size, fentrada);
+
+	char* salida = calloc(obtener_tamanio_encode(size), sizeof(char));
+
+	codificador64((unsigned char*)entrada, salida);
+	
+	fwrite(salida, 1, obtener_tamanio_encode(size), fsalida);
+
+	fclose(fentrada);
+	fclose(fsalida);
+	free(entrada);
+}
+
+void codificar_archivo_stdout(char nombre_fentrada[MAXIMO_ARCHIVO]){
+	FILE* fentrada = fopen(nombre_fentrada, "r");
+	if(!fentrada){
+		printf("No se pudo abrir al archivo de entrada.\n");
+		return;
+	}
+
+	fseek(fentrada, 0, SEEK_END); 
+	size_t size = (size_t) ftell(fentrada);
+	fseek(fentrada, 0, SEEK_SET); 
+
+	char* entrada = calloc(size + 1, sizeof(char));
+	if(!entrada){
+		fclose(fentrada);
+		return;
+	}
+
+	fread(entrada, 1, size, fentrada);
+
+	char* salida = calloc(obtener_tamanio_encode(size), sizeof(char));
+
+	codificador64((unsigned char*)entrada, salida);
+	
+	printf("%s", salida);
+
+	fclose(fentrada);
+	free(entrada);
+}
+
+void codificar_stdin_archivo(char nombre_fsalida[MAXIMO_ARCHIVO]){
+	FILE* fsalida = fopen(nombre_fsalida, "w");
+	if(!fsalida){
+		// TODO: ESCRIBIR ALGO.
+		return;
+	}
+	bool sigo_leyendo = true;
+	do{
+		char array_aEnco[4] = "";
+		char array_encodificado[5] = "";
+		scanf("%c%c%c", &array_aEnco[0], &array_aEnco[1], &array_aEnco[2]);
+		if(strlen(array_aEnco) == 3){
+			codificador64((unsigned char*)array_aEnco, array_encodificado);
+		}else if(strlen(array_aEnco) == 2){
+			codificador64((unsigned char*)array_aEnco, array_encodificado);
+			sigo_leyendo = false;
+		}else if(strlen(array_aEnco) == 1){
+			array_aEnco[2] = '\0';
+			codificador64((unsigned char*)array_aEnco, array_encodificado);
+			sigo_leyendo = false;
+		}else{
+			sigo_leyendo = false;
+		}
+		fwrite(array_encodificado, 1, obtener_tamanio_decode(strlen(array_encodificado)), fsalida);
+	}while(sigo_leyendo);
+}
+
+void codificar_stdin_stdout(){
+	bool sigo_leyendo = true;
+	do{
+		char array_aEnco[4] = "";
+		char array_encodificado[5] = "";
+		scanf("%c%c%c", &array_aEnco[0], &array_aEnco[1], &array_aEnco[2]);
+		if(strlen(array_aEnco) == 3){
+			codificador64((unsigned char*)array_aEnco, array_encodificado);
+		}else if(strlen(array_aEnco) == 2){
+			codificador64((unsigned char*)array_aEnco, array_encodificado);
+			sigo_leyendo = false;
+		}else if(strlen(array_aEnco) == 1){
+			array_aEnco[2] = '\0';
+			codificador64((unsigned char*)array_aEnco, array_encodificado);
+			sigo_leyendo = false;
+		}else{
+			sigo_leyendo = false;
+		}
+		printf("%s", array_encodificado);
+	}while(sigo_leyendo);
+}
+
+
+void mostrar_ayudas(){
+	printf("\n");
+	printf("-h, --help: Muestra la interfaz de ayudas.\n");
+	printf("-v, --version: Indica la version del programa.\n");
+	printf("-i, --input: Indica la direccion donde esta el archivo a leer.\n");
+	printf("-o, --output: Indica la direccion donde esta el archivo a escribir.\n");
+	printf("-d, --decode: Si se utiliza este flag se decodificara la entrada. De forma estandar la entrada es codificada.\n");
+	
+	printf("\n");
+	printf("Ejemplos:\n");
+	printf("\n");
+	printf("\n");
+
+	printf("> ./TP0 -i input.txt -o output.txt\n");
+	printf("\n");
+	
+	printf("> ./TP0 -i input.txt -o output.txt -d\n");
+	printf("\n");
+	
+	printf("> echo \"Eres debil Sasuke te falta odio.\" | ./TP0\n");
+	printf("> RXJlcyBkZWJpbCBTYXN1a2UgdGUgZmFsdGEgb2Rpby4K\n");
+	printf("\n");
+	
+	printf("> echo \"RXJlcyBkZWJpbCBTYXN1a2UgdGUgZmFsdGEgb2Rpby4K\" | ./TP0\n");
+	printf("> Eres debil Sasuke te falta odio.\n");
+	printf("\n");
+}
+
+void mostrar_version(){
+	printf("Version 1.0.0\n");
+}
+
+int main(int argc, char** argv){
+	int opt;
+	char archivo_entrada[MAXIMO_ARCHIVO] = "";
+	char archivo_salida[MAXIMO_ARCHIVO] = "";
+	bool decode = false;
+	bool pidio_info = false;
+
+	while((opt = getopt(argc, argv, "i:o:dvh")) != -1) {
+		switch(opt){
+			case 'i':
+				strcpy(archivo_entrada, optarg);
+			break;
+
+			case 'o':
+				strcpy(archivo_salida, optarg);
+			break;
+
+			case 'd':
+				decode = true;
+			break;
+
+			case 'h':
+				if(!pidio_info){
+					mostrar_ayudas();
+					pidio_info = true;
+				}
+			break;
+
+			case 'v':
+				if(!pidio_info){
+					pidio_info = true;
+					mostrar_version();
+				}
+			break;
+
+			default:
+			break;
+		}
+	}
+	
+	if(pidio_info){
+		return 0;
+	}
+	if(decode){
+		if(strlen(archivo_entrada) == 0 && strlen(archivo_salida) == 0){
+			decodificar_stdin_stdout();
+		}else if(strlen(archivo_entrada) != 0 && strlen(archivo_salida) == 0){
+			decodificar_archivo_stdout(archivo_entrada);
+		}else if(strlen(archivo_entrada) == 0 && strlen(archivo_salida) != 0){
+			decodificar_stdin_archivo(archivo_salida);
+		}else{
+			decodificar_archivo_archivo(archivo_entrada, archivo_salida);
+		}
+	}else{
+		if(strlen(archivo_entrada) == 0 && strlen(archivo_salida) == 0){
+			codificar_stdin_stdout();
+		}else if(strlen(archivo_entrada) != 0 && strlen(archivo_salida) == 0){
+			codificar_archivo_stdout(archivo_entrada);
+		}else if(strlen(archivo_entrada) == 0 && strlen(archivo_salida) != 0){
+			codificar_stdin_archivo(archivo_salida);
+		}else{
+			codificar_archivo_archivo(archivo_entrada, archivo_salida);
+		}
+	}		
+	return 0;
+}
