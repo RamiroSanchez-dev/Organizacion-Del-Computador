2d23a0476820ef0f9a6f5a9db32f1ed27b0a96b8
Lukas-De-Angelis-Riva <ldeangelis@fi.uba.ar>
2020-10-31 17:32:57 -0300

    Revert "Add files via upload"
    This reverts commit c13c03c56ebda635625b83e1b3ef96d2f60f9f12.

--- decode_encode.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DECODE_ENCODE_H__
-#define _DECODE_ENCODE_H__
-
-void codificador64(unsigned char* palabra, char* palabra_nueva);
-
-void decodificador64(unsigned char* palabra, char* palabra_nueva);
-
-#endif /* DECODE_ENCODE_H */
