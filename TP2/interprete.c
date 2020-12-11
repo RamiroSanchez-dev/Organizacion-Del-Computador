92f5bddd8cde9c1eb0202e8dee3d4d5fafd1de46
Lukas-De-Angelis-Riva <ldeangelis@fi.uba.ar>
2020-12-11 17:59:26 -0300

    feat: Esqueleto del intérprete
    Aún sin testear

--- TP2/interprete.c
+++ TP2/interprete.c
@@ -1,42 +1,42 @@
 #include "interprete.h"
 
+/* Para desechar la línea en caso de error */
+#define MAX_LINEA 100
 
-W ddddd, vvv
 void instruccion_escribir(char* instruccion){
 	int direccion = 0;
 	unsigned char byte = 0;
-	fscanf(archivo_entrada, "%i,%hhu\n", &direccion, &byte)
+	fscanf(archivo_entrada, "%i,%hhu\n", &direccion, &byte);
 	write_byte(direccion, byte);
 	fprintf(archivo_salida, "Escritura. --- %s\n",cache.hit?"Hit":"Miss");
 }
 
 void instruccion_leer(FILE* archivo_entrada,FILE* archivo_salida){
-    int direccion = 0;
-    fscanf(archivo_entrada, "%i\n",&direccion);
+	int direccion = 0;
+	fscanf(archivo_entrada, "%i\n",&direccion);
 	unsigned char byte = read_byte(direccion);
 	fprintf(archivo_salida,"Lectura: %hhu --- %s\n", byte,cache.hit?"Hit":"Miss");
 }
 
 void interpretar(FILE* archivo_entrada, FILE* archivo_salida){
-
-	bool sigo_leyendo = true;
-    char instruccion[5] = "";
-    //char direccion[16];
-    int i = 0;
-    while(feof(archivo_entrada)){
-        fscanf(archivo_entrada,"%s", instruccion);
-        if( strcmp(instruccion, "init")){
-            init();
-        }else if(strcmp(instruccion, "MR")){
-            int miss_rate = get_miss_rate();
-            fwrite(&miss_rate,1, sizeof(int),archivo_salida);
-        }else if (strcmp(instruccion, "R")){
-            instruccion_leer(archivo_entrada, archivo_salida);
-        }else if (strcmp(instruccion, "R")){
-            instruccion_escribir(archivo_entrada, archivo_salida);
-        }else{
-        	fprintf(stderr, "Error de sintaxis. Linea: %i", i);
-        }
-        i++;
-    }
+	char instruccion[5] = "";
+	int i = 0;
+	char tacho[MAX_LINEA];
+	while(!feof(archivo_entrada)){
+		fscanf(archivo_entrada,"%s", instruccion);
+		if(strcmp(instruccion, "init") == 0){
+			init();
+		}else if(strcmp(instruccion, "MR") == 0){
+			int miss_rate = get_miss_rate();
+			fwrite(&miss_rate,1, sizeof(int),archivo_salida);
+		}else if (strcmp(instruccion, "R") == 0){
+			instruccion_leer(archivo_entrada, archivo_salida);
+		}else if (strcmp(instruccion, "W") == 0){
+			instruccion_escribir(archivo_entrada, archivo_salida);
+		}else{
+			fscanf(archivo_entrada, "%s\n", tacho);
+			fprintf(stderr, "Error de sintaxis. Linea: %i", i);
+		}
+		i++;
+	}
 }
